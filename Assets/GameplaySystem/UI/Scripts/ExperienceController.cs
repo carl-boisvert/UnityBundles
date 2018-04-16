8e90bd052995c7fe0867b09af4f97a83368f6572
Carl Boisvert <cboisvert@ludia.com>
2018-04-16 17:00:53 -0400

    Add hp bar and XP bar

---
+++ Assets/GameplaySystem/UI/Scripts/ExperienceController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static partial class EventDelegate
+{
+    public delegate void ExperienceHandler(int exp);
+    public static event ExperienceHandler ExperienceEvent;
+    public static void OnExperienceEvent(int exp)
+    {
+        if (ExperienceEvent != null)
+        {
+            ExperienceEvent(exp);
+        }
+    }
+}
+public class ExperienceController : MonoBehaviour {
+
+    [SerializeField]
+    private Image radial;
+
+    private void OnEnable()
+    {
+        EventDelegate.ExperienceEvent += OnExperienceEvent;
+    }
+
+    private void OnDisable()
+    {
+        EventDelegate.ExperienceEvent -= OnExperienceEvent;
+    }
+
+    private void OnExperienceEvent(int exp)
+    {
+        radial.fillAmount = 0.5f;
+    }
+}
---
+++ Assets/GameplaySystem/UI/Scripts/ProgressBarController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarController : MonoBehaviour {
+
+    [SerializeField]
+    protected float maxValue = 100;
+    [SerializeField]
+    protected float currentValue = 100;
+    protected RawImage ui;
+
+    protected void UpdateUi()
+    {
+        float percent = currentValue / maxValue;
+        float xValue = -((percent * 0.5f) - 0.5f);
+        ui.uvRect = new Rect(xValue, 0f, 0.5f, 1f);
+    }
+}
