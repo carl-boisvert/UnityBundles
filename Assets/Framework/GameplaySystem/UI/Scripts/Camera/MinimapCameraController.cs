2aaec67cc5b9fcb91ae1384b1893b4b845f27732
Carl Boisvert <cboisvert@ludia.com>
2018-04-27 18:44:42 -0400

    Add minimap WIP

---
+++ Assets/Framework/GameplaySystem/UI/Scripts/Camera/MinimapCameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class MinimapCameraController : MonoBehaviour {
+
+    [SerializeField]
+    private float cameraDistance = 5.0f;
+    private Camera camera;
+    private Transform playerPosition;
+
+	// Use this for initialization
+	void Start () {
+        camera = GetComponent<Camera>();
+        playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+        camera.transform.position = playerPosition.position + new Vector3(0, cameraDistance, 0);
+        camera.transform.LookAt(playerPosition);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
